9c80701fd0a12f0d2a1d3292677b11f9cbf8fe8a
Dylan Leard <dleard@uvic.ca>
2019-01-15 18:35:32 +0000

    added urls_index.ejs in views folder

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
@@ -11,8 +13,9 @@ app.get('/', (req,res) => {
   res.send("Hello. You are at the root!");
 });
 
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
+app.get('/urls', (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
 
 app.get('/hello', (req, res) => {
