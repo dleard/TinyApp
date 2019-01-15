9a3155bfb3be244f1c0cdb0c0e4179d8b969161a
Dylan Leard <dleard@uvic.ca>
2019-01-15 18:17:25 +0000

    added express package and express_server.js file

---
+++ express_server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+const urlDatabase = {
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com'
+};
+
+app.get('/', (req,res) => {
+  res.send("Hello. You are at the root!");
+});
+
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n')
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
